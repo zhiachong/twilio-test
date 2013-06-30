42f51b7fc03172ddbdd3051838ceb135168594d1
Zhia Chong <zhiachong@gmail.com>
2013-06-29 17:01:55 -0700

    new num added

--- run.py
+++ run.py
@@ -9,9 +9,10 @@ def hello_monkey():
         server = smtplib.SMTP ("smtp.gmail.com", 587)
         server.starttls()
         server.login('zhiachong@gmail.com', 'chongzhiahwa')
-        numberFormat = '17164796637' + '@tmomail.net'
+        numberFormat = '4048610464' + '@mms.att.net'
         server.sendmail('123112312', numberFormat, 'damn')
         
+
 
 if __name__ == "__main__":
     app.run(debug=True)
