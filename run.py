b6bb34735d192baa69be5cef34ff54e6df382af9
Zhia Chong <zhiachong@gmail.com>
2013-06-29 18:18:33 -0700

    add reply

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey(): 
+    #todo
+    #get phone number & msg from incoming msg
+    #know which phone number to send to by querying the database
+    #finally, send the msg
+
     account_sid = "ACc34c6db115eb6f7fa3ffb5efb10f2ec3"
     auth_token = "07ec4e439d0025ae71b28509cc801857"
     client = TwilioRestClient(account_sid, auth_token)
